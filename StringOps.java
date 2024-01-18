db261271474d454a9e3a8db31d82f6088a3bac59
RonIsakov <154628459+RonIsakov@users.noreply.github.com>
2024-01-18 17:50:02 +0200

    Update StringOps.java

--- StringOps.java
+++ StringOps.java
@@ -1,119 +1,80 @@
-public class StringOps {
-    ////////////////////////////////////////////////////////////
-    //////                                               ///////
-    //////              R eminder:                        ///////
-    //////        allowed methods                        ///////
-    //////                                               ///////
-    //////        1.charAt(int index)                    ///////
-    //////        2.length()                             ///////
-    //////        3.substring(int start)                 ///////
-    //////        4.substring(int start,int ends)        ///////
-    //////        5.indexOf(String str)                  ///////
-    //////                                               ///////
-    //////        The rest are not allowed !             ///////
-    //////        if you want to use a different         ///////
-    //////        method, and you can implement          ///////
-    //////        it using material from the course      ///////
-    //////        you need to implement a version of     ///////
-    //////        the function by yourself.              ///////
-    //////                                               ///////
-    //////        see example for substring              ///////
-    //////        in Recitation 3 question 5             ///////
-    //////                                               ///////
-    ////////////////////////////////////////////////////////////
+public class ArrayOps {
     public static void main(String[] args) {
-    String check = "One two tHRee world";
-    System.out.println(capVowelsLowRest(check));
-    String check2 = "   One two tHRee world";
-    System.out.println(camelCase(check2));
-    String check3 = "MMMM";
-    char M ='M';
-    int []  test = new int[4];
-    test = allIndexOf(check3,M);
-    for (int i=0;i<4; i++){
-        System.out.print(test [i] + " ");
+        
     }
-
-    }
-
-    //makes all the vowels caps the rest lowCaps
-    public static String capVowelsLowRest (String string) {
-        String Str = "";
-        String allVowels = "aeiouAEIOU";
-        for(int i=0 ; i<string.length() ; i++) {
-            int n = string.charAt(i);
-            //chekinf if the char is a vowel
-            if(allVowels.indexOf(n) != -1) {
-                if(string.charAt(i)>90)
-                    Str = Str + (char)(n - 32);
-                else
-            Str = Str + string.charAt(i);
-            }
-            //making all the rest upper case lower case skipping ' '
-            else if(string.charAt(i) == ' ' || n > 90){
-                    Str = Str + string.charAt(i);
-                    }
-                    else{
-                        Str = Str + (char)(n + 32);
-                    }
-        }
-        return (Str);
-        }
     
-    // make the input a single word instead of spaces making the first later after caps
-    public static String camelCase (String string) {
-        String Str = "";
-        int counter = 0;
-        //making the string lower case
-        string = lowCase(string);
-        while(string.charAt(counter) == ' ' && counter < string.length() ){
-        counter++;
+    //takes an array of integers {0,1,2...n} that is missing one integer, and return the value of the missing integer
+    public static int findMissingInt (int [] array) {
+        int n = array.length;
+        //declaring an array that holds true or false for each index if the corresponding index exist as value in the array or not
+        boolean check[] = new boolean[n+1];
+        for(int i=0 ; i<n ; i++) 
+            check[i] = false;
+        for(int i=0 ; i<n ; i++) {
+            check[array[i]] = true;
         }
-        //going through the string making it without spaces making the first latter after a space caps
-        for(int i = counter ;i<string.length() ; i++) {
-            if(string.charAt(i) == ' '){
-                while(string.charAt(i) == ' ' && i < string.length() ){
-                    i++;
-                    }
-                    Str = Str + ((char)(string.charAt(i) - 32));
-                }
-                else{
-                    Str = Str + string.charAt(i);
-                }
-    }
-    return(Str);
+        int j = 0;
+        while(check[j])
+            j++;
+        return j;
     }
 
-
-
-    //taking a string making it all low caps
-    public static String lowCase(String string) {
-        String Str = "";
-        for(int i = 0 ; i < string.length() ; i++)
-            if(string.charAt(i) <= 90 && string.charAt(i) != ' ')
-                Str = Str + (char)(string.charAt(i)+32);
-            else
-                Str += string.charAt(i);
-        return Str;
-    }
-
-    // taking a string and a char  and returning the indexes of the string in which the char appears in an array
-    public static int[] allIndexOf (String string, char chr) {
-        int counter = 0;
-        int indexcounter=0;
-        //checking how many times chr is in the string
-        for(int i=0 ; i<string.length() ; i++) {
-            if(string.charAt(i)==chr)
-                counter++;
-        }
-        int[] Index = new int[counter];
-        //making an array that holds all the indexes
-        for(int i=0 ; i<string.length() ; i++) {
-            if(string.charAt(i)==chr) {
-                Index[indexcounter] = i;
-                indexcounter++;
+    //takes an array of integers as input and return the 2nd biggest integer in the array (return the 1st biggest integer if it appears more than once)
+    public static int secondMaxValue(int [] array) {
+        //declaring and initializing max and max2 as the smallest value of integer type variable
+        int max = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        int maxIndex = 0;
+        //searching for the max value
+        for(int i=0 ; i<array.length ; i++) {
+            if(max<array[i]) {
+                max = array[i];
+                maxIndex = i;
             }
         }
-        return Index;
+        //searching for the max without looking at the index of 'max'
+        for(int i=0 ; i<array.length ; i++) {
+            if(max2 < array[i] && i != maxIndex)
+                max2 = array[i];
+        }
+        return max2;
     }
+
+    //takes 2 arrays of integers as input and return true if they contain the same integers (ignoring duplicates)
+    public static boolean containsTheSameElements(int [] array1,int [] array2) {
+        boolean check = true;
+        //checking if all of array1's integers appear in array2
+        for(int i=0 ; i<array1.length ; i++) {
+            for(int j=0 ; j<array2.length ; j++) {
+                if(array1[i]==array2[j])
+                    break;
+                else if(j==array2.length-1)
+                    check = false;
+            }
+        }
+        //checking if all of array2's integers appear in array1
+        for(int i=0 ; i<array2.length ; i++) {
+            for(int j=0 ; j<array1.length ; j++) {
+                if(array2[i]==array1[j])
+                    break;
+                else if(j==array1.length-1)
+                    check = false;
+            }
+        }
+        return check;
+    }
+
+    // takes an array of integers as input and return true if the array is either increasing in value or decreasing as you go from index 0 to n
+    //ignoring equal values that are near eachother
+    public static boolean isSorted(int [] array) {
+        boolean increase=true, decrease=true;
+        for(int i=0 ; i<array.length-1 && (increase || decrease); i++) {
+            if(array[i]>array[i+1] && increase)
+                increase = false;
+            if(array[i]<array[i+1] && decrease)
+                decrease = false;
+        }
+        return increase || decrease;
+    }
+
 }
