3bfe8b9826d41586d53ceb7a1190d8c07f590737
RonIsakov <ron.isakov@post.runi.ac.il>
2024-01-18 16:22:43 +0200

    try 3

--- StringOps.java
+++ StringOps.java
@@ -22,88 +22,88 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        String test1 = "one two tHRee world";
-        System.out.println(capVowelsLowRest(test1));
-        String test2 = "  Intro to  coMPUter   sCIEncE   ";
-        System.out.println(camelCase(test2));
+    String check = "lOve You ALLLL";
+    System.out.println(capVowelsLowRest(check));
     }
-    
-    
+
+    //makes all the vowels caps the rest lowCaps
     public static String capVowelsLowRest (String string) {
-    String newStr = "";
-    char n;
-    // cheking if each char of the string is a vowel if it is making it uper case 
-    // if a letter witch is not a vowel is in upper case making it lower case returning the new string
-    for(int i = 0; i < string.length(); i++ ){
-        n = string.charAt(i);
-        //cheking if there is a low caps vowel making it caps
-        if( n == 'a' || n == 'e' ||  n == 'o' || n == 'u' || n == 'i' ){
-            newStr = (newStr + (char) (n - 32));
-            //cheking if there is a caps letter expt vowel making it lower case
-        }
-        else if(65 <= n && n <= 90 && n != 'A' && n != 'O'  && n != 'U'  && n != 'I'  && n != 'E'){
-            newStr = (newStr + (char) (n + 32));
-        }
-        // if its not all the above then puting it in the new string
-        else{
-            newStr = newStr + n;
-        }
-    }
-        return (newStr);
-    }
-
-    public static String camelCase (String string) {
-    String newStr = "";
-    char n;
-    int counter =0;
-    // deliting all the spaces befor the first word
-        if (string.charAt(0) == ' '){
-            while(string.charAt(counter) == ' '  && counter < string.length())
-            counter++;
-        }
-        // cheking each letter making it lower case if 
-    //there are sapces deliting them and making the next letter upercase
-    for (int i = counter; i < string.length(); i++) {
-        n = string.charAt(i);
-        if (65 <= n && n <= 90) {
-            newStr = (newStr + (char) (n + 32));
-        } else if (n == ' ') {
-            // Skip consecutive spaces
-            while (i < string.length() && string.charAt(i) == ' ') {
-                i++;
+        String Str = "";
+        String allVowels = "aeiouAEIOU";
+        for(int i=0 ; i<string.length() ; i++) {
+            int n = string.charAt(i);
+            //chekinf if the char is a vowel
+            if(allVowels.indexOf(n) != -1) {
+                if(string.charAt(i)>90)
+                    Str = Str + (char)(n - 32);
+                else
+            Str = Str + string.charAt(i);
             }
-
-            // Convert the next character to uppercase
-            if (i < string.length() ) {
-                n = string.charAt(i);
-                newStr = (newStr + (char) (n - 32));
-            }
-        } else {
-            newStr = newStr + n;
+            //making all the rest upper case lower case skipping ' '
+            else if(string.charAt(i) == ' ' || n > 90){
+                    Str = Str + string.charAt(i);
+                    }
+                    else{
+                        Str = Str + (char)(n + 32);
+                    }
         }
-    }
+        return (Str);
+        }
     
-        return (newStr);
+    // takes a (valid) string as input and return the same string without spaces and all lower case
+    // except for the first letter of each word from the second word (incusive) and on
+    public static String camelCase (String string) {
+        String Str = "";
+        int counter = 0;
+        string = lowCase(string);
+        while(string.charAt(counter) == ' ' && counter < string.length() ){
+        counter++;
+        }
+        //going through the string char by char and mutating it according to the requirments
+        for(int i = i ;i<string.length() ; i++) {
+            if(string.charAt(i) == ' '){
+                while(string.charAt(i) == ' ' && i < string.length() ){
+                    i++;
+                    }
+                    Str = Str + ((char)(string.charAt(i) - 32));
+                }
+                else{
+                    Str = Str + string.charAt(i);
+                }
+    }
+    return(Str);
     }
 
+
+
+    //take a (valid) string as input and return the same string with only lower cases letters
+    public static String lowCase(String string) {
+        String Str = "";
+        for(int i = 0 ; i < string.length() ; i++)
+            if(string.charAt(i) <= 90 && string.charAt(i) != ' ')
+                Str = Str + (char)(string.charAt(i)+32);
+            else
+                Str += string.charAt(i);
+        return Str;
+    }
+
+    //takes a (valid) string and a char as input and return the indexes of the string in which the char appears
     public static int[] allIndexOf (String string, char chr) {
         int counter = 0;
-        char n;
-        for(int i = 0; i < string.length(); i++ ){
-            if(chr == string.charAt(i)){
+        //checking how many times chr appears in string
+        for(int i=0 ; i<string.length() ; i++) {
+            if(string.charAt(i)==chr)
                 counter++;
+        }
+        int j=0;
+        //declaaring an array to hold the wanted indexes
+        int[] chrIndex = new int[counter];
+        for(int i=0 ; i<string.length() ; i++) {
+            if(string.charAt(i)==chr) {
+                chrIndex[j] = i;
+                j++;
             }
         }
-
-        int counterArr = 0;
-        int [] arr =new int [counter];
-        for(int i = 0; i < string.length(); i++ ){
-            n = string.charAt(i);
-            if(n == chr){
-                arr [counterArr] = i ;
-                counterArr++;
-            }
-        }
-        return(arr);
+        return chrIndex;
     }
 }
